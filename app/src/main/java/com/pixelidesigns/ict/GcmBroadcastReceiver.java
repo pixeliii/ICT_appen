2b7d2c748474adf9c075e934b6f5a076e72569c4
Axel Andersin <axel.andersin@gmail.com>
2015-03-16 14:51:25 +0200

    Initial commit

---
+++ app/src/main/java/com/pixelidesigns/ict/GcmBroadcastReceiver.java
@@ -0,0 +1,31 @@
+package com.pixelidesigns.ict;
+
+/**
+ * Created by Axel on 16.3.2015.
+ */
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GcmBroadcastReceiver  extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+
+}
+
+
+
+}
+
+
+
+
---
+++ app/src/main/java/com/pixelidesigns/ict/GcmIntentService.java
@@ -0,0 +1,75 @@
+package com.pixelidesigns.ict;
+
+/**
+ * Created by Axel on 16.3.2015.
+ */
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+
+public class GcmIntentService extends IntentService {
+    public static final int NOTIFICATION_ID = 1;
+    private NotificationManager mNotificationManager;
+    NotificationCompat.Builder builder;
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
+
+    public static final String TAG = "GCM Demo";
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        String messageType = gcm.getMessageType(intent);
+
+        if (!extras.isEmpty()) {
+
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                sendNotification("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                for (int i = 0; i < 5; i++) {
+                    Log.i(TAG, "Working..." + (i + 1)
+                            + "/5 @" + SystemClock.elapsedRealtime());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+                Log.i(TAG, "Complete work @ " + SystemClock.elapsedRealtime());
+                sendNotification("Received: " + extras.toString());
+                Log.i(TAG, "Received: " + extras.toString());
+            }
+        }
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+private void sendNotification(String msg) {
+    mNotificationManager = (NotificationManager)
+            this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+    PendingIntent contenctIntent = PendingIntent.getActivity(this, 0,
+            new Intent(this, MainActivity.class),0);
+
+    NotificationCompat.Builder mBuilder =
+            new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.ic_stat_gcm)
+            .setContentTitle("GCM Notification")
+            .setStyle(new NotificationCompat.BigTextStyle()
+            .bigText(msg))
+            .setContentText(msg);
+
+            mBuilder.setContentIntent(contentIntent);
+    }
+}
